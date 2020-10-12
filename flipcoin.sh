83f7389d91914e18912dd9784895be081d646914
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-12 10:57:21 +0530

    head and tail in coin added and made for tripleton and merged

--- flipcoin.sh
+++ flipcoin.sh
@@ -83,3 +83,94 @@ echo " percentage of tail_head combination:" $percentageofTailHead"%"
 echo " percentage of tail_tail combination:" $percentageofTailTail"%"
 
 }
+#triplet combination
+tripletcombination ( ) {
+
+coin[headheadhead]=0
+coin[headheadtail]=0
+coin[headtailhead]=0
+coin[headtailtail]=0
+coin[tailheadhead]=0
+coin[tailheadtail]=0
+coin[tailtailhead]=0
+coin[tailtailtail]=0
+
+for (( i=0; i<$loop; i++ ))
+do
+firstcoinflip=$((RANDOM%2))
+secondcoinflip=$((RANDOM%2))
+thirdcoinflip=$((RANDOM%2))
+
+if [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 0 ]
+then
+        echo " it is a head_head_head combination"
+        coin[headheadhead]=$(( ${coin[headheadhead]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 1 ]
+then
+        echo "it is a head_head_tail combination"
+        coin[headheadtail]=$(( ${coin[headheadtail]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 0  ]
+then
+        echo "it is a head_tail_head combination"
+        coin[headtailhead]=$(( ${coin[headtailhead]}+1 ))
+
+elif [ $firstcoinflip -eq 0 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 1  ]
+then
+        echo "it is a head_tail_tail combination"
+        coin[headtailtail]=$(( ${coin[headtailtail]}+1 ))
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 0 ]
+then
+        echo " it is a tail_head_head combination"
+        coin[tailheadhead]=$(( ${coin[tailheadhead]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 0 -a $thirdcoinflip -eq 1 ]
+then
+        echo "it is a tail_head_tail combination"
+        coin[tailheadtail]=$(( ${coin[tailheadtail]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 0  ]
+then
+        echo "it is a tail_tail_head combination"
+        coin[tailtailhead]=$(( ${coin[tailtailhead]}+1 ))
+
+elif [ $firstcoinflip -eq 1 -a $secondcoinflip -eq 1 -a $thirdcoinflip -eq 1  ]
+then
+        echo "it is a head_tail_tail combination"
+        coin[tailtailtail]=$(( ${coin[tailtailtail]}+1 ))
+
+fi
+done
+
+echo " number of head_head_head combination flips" ${coin[headheadhead]}
+echo " number of head_head_tail combination flips" ${coin[headheadtail]}
+echo " number of head_tail_head combination flips" ${coin[headtailhead]}
+echo " number of head_tail_tail combination flips" ${coin[headtailtail]}
+echo " number of tail_head_head combination flips" ${coin[tailheadhead]}
+echo " number of tail_head_tail combination flips" ${coin[tailheadtail]}
+echo " number of tail_tail_head combination flips" ${coin[tailtailhead]}
+echo " number of tail_tail_tail combination flips" ${coin[tailtailtail]}
+
+sumofcombinations=$(( ${coin[headheadhead]}+${coin[headheadtail]}+${coin[headtailhead]}+${coin[headtailtail]}+ ${coin[tailheadhead]}+${coin[tailheadtail]}+${coin[tailtailhead]}+${coin[tailtailtail]} ))
+percentageofHeadHeadHead=$(( (${coin[headheadhead]}*100)/$sumofcombinations ))
+percentageofHeadHeadTail=$(( (${coin[headheadtail]}*100)/$sumofcombinations ))
+percentageofHeadTailHead=$(( (${coin[headtailhead]}*100)/$sumofcombinations ))
+percentageofHeadTailTail=$(( (${coin[headtailtail]}*100)/$sumofcombinations ))
+percentageofTailHeadHead=$(( (${coin[tailheadhead]}*100)/$sumofcombinations ))
+percentageofTailHeadTail=$(( (${coin[tailheadtail]}*100)/$sumofcombinations ))
+percentageofTailTailHead=$(( (${coin[tailtailhead]}*100)/$sumofcombinations ))
+percentageofTailTailTail=$(( (${coin[tailtailtail]}*100)/$sumofcombinations ))
+
+
+echo " percentage of head_head_head combination:" $percentageofHeadHeadHead"%"
+echo " percentage of head_head_tail combination:" $percentageofHeadHeadTail"%"
+echo " percentage of head_tail_head combination:" $percentageofHeadTailHead"%"
+echo " percentage of head_tail_tail combination:" $percentageofHeadTailTail"%"
+echo " percentage of tail_head_head combination:" $percentageofTailHeadHead"%"
+echo " percentage of tail_head_tail combination:" $percentageofTailHeadTail"%"
+echo " percentage of tail_tail_head combination:" $percentageofTailTailHead"%"
+echo " percentage of tail_tail_tail combination:" $percentageofTailTailTail"%"
+
+
+}
