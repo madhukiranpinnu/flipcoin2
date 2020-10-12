0a6680400584260f862a8fe1e52649963bb0d31f
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-12 10:42:29 +0530

    head and tail in coin added

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo "welcome to flipcoin"
+coinvalue=$(( RANDOM%2 ));
+        if [ $coinvalue  -eq 1 ]
+        then
+             echo "head"
+        else
+              echo "tail"
+        fi
